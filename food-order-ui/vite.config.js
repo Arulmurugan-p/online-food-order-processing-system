31299459f4de8f387c79a027495aaf992e7b235e
Arulmurugan-p <arulmuruganhari768@gamil.com>
2026-07-11 11:34:19 +0530

    Configure React UI project for GitHub Pages deployment

--- food-order-ui/vite.config.js
+++ food-order-ui/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/online-food-order-processing-system/',
   plugins: [react()],
   server: {
     port: 5173
